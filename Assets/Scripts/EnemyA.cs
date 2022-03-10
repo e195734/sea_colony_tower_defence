b6fbe2658adda70158a651308b20ed3037a6941a
Naoru Chinen <c.naoru0706@gmail.com>
2022-03-11 01:40:18 +0900

    タコがコロニーを追跡するように。

--- Assets/Scripts/EnemyA.cs
+++ Assets/Scripts/EnemyA.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class EnemyA : MonoBehaviour
 {
     private HP hp = new HP(5);
-
+    private float speed = 1f;
+    private GameObject target = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Colony");
     }
 
     // Update is called once per frame
@@ -20,7 +21,10 @@ public class EnemyA : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        if (target)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        }
     }
     private bool isDeath()
     {
